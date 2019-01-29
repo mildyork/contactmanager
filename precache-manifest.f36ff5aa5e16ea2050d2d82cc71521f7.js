37aa80004463b86a47f3d2cea34bb65b385bda8a
York Zhang <yorkcheung@hotmail.com>
2019-01-29 14:26:26 +0800

    Updates

---
+++ precache-manifest.f36ff5aa5e16ea2050d2d82cc71521f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "0ac7f898a81dfbacf556",
+    "url": "/contactmanager/static/js/main.0ac7f898.chunk.js"
+  },
+  {
+    "revision": "50efb99a9bad8b555e5e",
+    "url": "/contactmanager/static/js/1.50efb99a.chunk.js"
+  },
+  {
+    "revision": "0ac7f898a81dfbacf556",
+    "url": "/contactmanager/static/css/main.4d06353b.chunk.css"
+  },
+  {
+    "revision": "50efb99a9bad8b555e5e",
+    "url": "/contactmanager/static/css/1.e8b736f9.chunk.css"
+  },
+  {
+    "revision": "8c44d550c64e47abf5a192323ae9eff4",
+    "url": "/contactmanager/index.html"
+  }
+];
